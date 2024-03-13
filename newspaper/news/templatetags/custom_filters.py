7c06a33c7c597dabda9fb1191d876074b383714f
Hriakova-Mariya <137207701+Hriakova-Mariya@users.noreply.github.com>
2024-03-13 21:26:18 +0300

    Update custom_filters.py

--- newspaper/news/templatetags/custom_filters.py
+++ newspaper/news/templatetags/custom_filters.py
@@ -2,11 +2,11 @@ from django import template
 
 register = template.Library()
 
-CENSOR_WORD= ['Спорт', 'темы', 'пломбы']
+CENSOR_WORD= ['Спорт', 'социальных', 'пломбы']
 
 @register.filter()
 def censor(value):
     for word in CENSOR_WORD:
-        value.replace(word[1:],'*' *len(word[1:]))
+        value = value.replace(word[1:],'*' *len(word[1:]))
         return value
 
